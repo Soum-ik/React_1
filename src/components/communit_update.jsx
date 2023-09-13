dcdd74e0c76af0766fd88ee1d8ac3a656666dc85
soum-ik <sarkarsoumik215@gmail.com>
2023-09-13 23:19:53 +0600

    final

--- src/components/communit_update.jsx
+++ src/components/communit_update.jsx
@@ -32,22 +32,22 @@ export default function Community() {
         </div>
       </div>
       <div className="px-4 mt-8 max-w-screen-2xl mx-auto mb-12">
-              <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-between gap-8 items-center pt-4">
-                  {Images.map((image) => (
-                    <div
-                      key={image.id}
-                      className="mx-auto relative mb-12 cursor-pointer "
-                    >
-                      <img src={image.Image} alt="" />
-                      <div className=" text-center mx-auto border-md px-4 py-4 bg-slate-100   shadow-md hover:shadow-lg rounded-sm max-w-xs md:w-3/4 left-0 right-0 absolute -bottom-12">
-                        <h1 className="text-neutralGrey text-lg pb-4 ">
-                          {image.tittle}
-                        </h1>
-                        <button className="buttom">Read more..</button>
-                      </div>
-                    </div>
-                  ))}
-                </div>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-between gap-8 items-center pt-4">
+          {Images.map((image) => (
+            <div
+              key={image.id}
+              className="mx-auto relative mb-12 cursor-pointer "
+            >
+              <img src={image.Image} alt="" />
+              <div className=" text-center mx-auto border-md px-4 py-4 bg-slate-100   shadow-md hover:shadow-lg rounded-sm max-w-xs md:w-3/4 left-0 right-0 absolute -bottom-12">
+                <h1 className="text-neutralGrey text-lg pb-4 ">
+                  {image.tittle}
+                </h1>
+                <button className="buttom">Read more..</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
